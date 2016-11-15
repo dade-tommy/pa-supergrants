80db5b84fa4012af5a61f64431b181e46a72e7a2
Thomas Lenz <bf4ktommy@gmail.com>
2016-11-15 21:27:52 +0100

    Update grants.lua

--- data/grants.lua
+++ data/grants.lua
@@ -9,12 +9,12 @@ function CreateMassEmployment()
 
 	Objective.CreateGrant                   ( "Grant_MassEmployment", 0, 2000 )
 	
-	Objective.CreateGrant			( "Grant_MyGrant_MyRequirement1", 0, 0 )
-	Objective.SetParent			( "Grant_MyGrant" )
+	Objective.CreateGrant			( "Grant_MassEmployment_MyRequirement1", 0, 0 )
+	Objective.SetParent			( "Grant_MassEmployment" )
 	Objective.RequireRoom			( "Staffroom", true )
 	
-	Objective.CreateGrant			( "Grant_MyGrant_MyRequirement2", 0, 0 )
-	Objective.SetParent			( "Grant_MyGrant" )
+	Objective.CreateGrant			( "Grant_MassEmployment_MyRequirement2", 0, 0 )
+	Objective.SetParent			( "Grant_MassEmployment" )
 	Objective.RequireObjects                ( "Policeman", 30 )
 end
 
